e9b7664c902256bacc69bc7fd7e69c9171b118be
Prathamesh111-netizen <2002pratham1109@gmail.com>
2023-11-03 12:23:32 +0530

    initial commmit

--- tests/Ballot_test.sol
+++ tests/Ballot_test.sol
@@ -12,17 +12,14 @@ contract BallotTest {
     Ballot ballotToTest;
     function beforeAll () public {
         proposalNames.push(bytes32("candidate1"));
-        proposalNames.push(bytes32("candidate2"));
         ballotToTest = new Ballot(proposalNames);
     }
 
     function checkWinningProposal () public {
         console.log("Running checkWinningProposal");
-        ballotToTest.vote(1);
+        ballotToTest.vote(0);
         Assert.equal(ballotToTest.winningProposal(), uint(0), "proposal at index 0 should be the winning proposal");
         Assert.equal(ballotToTest.winnerName(), bytes32("candidate1"), "candidate1 should be the winner name");
-        Assert.equal(ballotToTest.winningProposal(), uint(1), "proposal at index 0 should be the winning proposal");
-        Assert.equal(ballotToTest.winnerName(), bytes32("candidate2"), "candidate1 should be the winner name");
     }
 
     function checkWinninProposalWithReturnValue () public view returns (bool) {
